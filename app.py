79b4a4350982c3fe847e544d15d22f7198bf017a
Tolani Afolabi <tolaniafolabi24@gmail.com>
2019-06-05 20:51:23 -0500

    commented out the dependencies not needed in app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-import os
+# import os
 
 import pandas as pd
-import numpy as np
+# import numpy as np
 
-import sqlalchemy
+# import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+# from sqlalchemy.orm import Session
+# from sqlalchemy import create_engine
 
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
@@ -18,7 +18,7 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:/db/bellybutton.sqlite"
 db = SQLAlchemy(app)
 
 # reflect an existing database into a new model

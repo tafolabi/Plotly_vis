f7999e8a3b7743793e5417be03f940989a8aafc3
Tolani Afolabi <tolaniafolabi24@gmail.com>
2019-06-05 22:03:24 -0500

    created pie chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,57 +1,35 @@
 
-// var url = "http://127.0.0.1:5000/names";
-var sample = "1551";
-console.log(`/samples/${sample}`);
-d3.json(`/samples/${sample}`).then(function(data) { 
-// var metaSample = d3.select("#sample-metadata");
-console.log(data);
-  });
-// function buildMetadata() {
-//   d3.json(url).then(function(data) { 
-//     var metaSample = d3.select("#sample-metadata")};
-//     console.log(data);
-//     });
 
-//     function buildPlot() {
-//       d3.json(url).then(function(data) {
+
+// console.log(`/samples/${sample}`);
+// d3.json(`/samples/${sample}`).then(function(data) { 
+// // var metaSample = d3.select("#sample-metadata");
+// console.log(data);
+//   });
+function buildMetadata() {
+  var url = `/metadata/${sample}`;
+  d3.json(url).then(function(data) { 
+  
+    console.log(data);
+    var metaSample = d3.select("#sample-metadata");
+    metaSample.html("")
+    Object.enteries(data).forEach(([key,value]) => {
+      metaSample.append("h6").text(`${key}:${value}`);
+  });
+
+
+
+
+
+
+      buildGauge(data.WFREQ);
+  
+});
+};
+  
+      
     
-//         // Grab values from the data json object to build the plots
-//         var name = data.dataset.name;
-//         var stock = data.dataset.dataset_code;
-//         var startDate = data.dataset.start_date;
-//         var endDate = data.dataset.end_date;
-//         var dates = unpack(data.dataset.data, 0);
-//         var closingPrices = unpack(data.dataset.data, 1);
-    
-//         var trace1 = {
-//           type: "scatter",
-//           mode: "lines",
-//           name: name,
-//           x: dates,
-//           y: closingPrices,
-//           line: {
-//             color: "#17BECF"
-//           }
-//         };
-    
-//         var data = [trace1];
-    
-//         var layout = {
-//           title: `${stock} closing prices`,
-//           xaxis: {
-//             range: [startDate, endDate],
-//             type: "date"
-//           },
-//           yaxis: {
-//             autorange: true,
-//             type: "linear"
-//           }
-//         };
-    
-//         Plotly.newPlot("plot", data, layout);
-    
-//       });
-//     }
+
     
 //     buildPlot();
 // //   // @TODO: Complete the following function that builds the metadata panel
@@ -74,11 +52,29 @@ console.log(data);
 // //     // Hint: Inside the loop, you will need to use d3 to append new
 // //     // tags for each key-value in the metadata.
 
-// //     // BONUS: Build the Gauge Chart
-// //     // buildGauge(data.WFREQ);
-// // }
+    // BONUS: Build the Gauge Chart
+//   buildGauge(data.WFREQ);
+  
+//   }):
+// }
 
-// // function buildCharts(sample) {
+ function buildCharts(sample) {
+  var url = `/samples/${sample}`;
+  d3.json(url).then(function(data) { 
+  
+    console.log(data);
+    
+    var data = [
+      {
+      labels:data ["otu_ids"].slice (0,10),
+      values:data["sample_values"].slice(0,10),
+      hovertext:data["otu_labels"].slice(0,10),
+      type: "pie"
+  }];
+  var layout = {title: "Bacteria Pie Chart",
+
+  };
+  Plotly.newPlot("pie",data,layout);
 
 // //   // @TODO: Use `d3.json` to fetch the sample data for the plots
 
@@ -87,33 +83,34 @@ console.log(data);
 // //     // @TODO: Build a Pie Chart
 // //     // HINT: You will need to use slice() to grab the top 10 sample_values,
 // //     // otu_ids, and labels (10 each).
-// // }
+});
+ }
 
-// // function init() {
-// //   // Grab a reference to the dropdown select element
-// //   var selector = d3.select("#selDataset");
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
 
-// //   // Use the list of sample names to populate the select options
-// //   d3.json("/names").then((sampleNames) => {
-//     sampleNames.forEach((sample) => {
-//       selector
-//         .append("option")
-//         .text(sample)
-//         .property("value", sample);
-//     });
+//   // Use the list of sample names to populate the select options
+  d3.json("/names").then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
 
-//     // Use the first sample from the list to build the initial plots
-//     const firstSample = sampleNames[0];
-//     buildCharts(firstSample);
-//     buildMetadata(firstSample);
-//   });
-// }
+    // Use the first sample from the list to build the initial plots
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
 
-// function optionChanged(newSample) {
-//   // Fetch new data each time a new sample is selected
-//   buildCharts(newSample);
-//   buildMetadata(newSample);
-// }
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildCharts(newSample);
+  buildMetadata(newSample);
+}
 
-// // Initialize the dashboard
-// init();
+//  Initialize the dashboard
+init();
